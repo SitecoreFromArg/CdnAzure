7d58464adea58d602f7f2d36a9cbafec76a929fe
Lautaro Bonetto <lautaro.bonetto@gmail.com>
2014-10-24 20:21:43 -0300

    Added first version

---
+++ CdnAzureProcessor.cs
@@ -0,0 +1,93 @@
+﻿using System.IO;
+using System.Text;
+using Sitecore;
+using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
+using System.Web;
+
+namespace SitecoreFromArg.CdnAzure
+{
+    public class CdnAzureProcessor
+    {
+        public string CdnSourceDomain { get; set; }
+
+        public CdnAzureProcessor(string cdnSourceDomain)
+        {
+            var cdnFolderPath = GetCdnFolderPath(cdnSourceDomain);
+            if (!Directory.Exists(cdnFolderPath)) Directory.CreateDirectory(cdnFolderPath);
+            CdnSourceDomain = cdnSourceDomain;
+        }
+
+        public void UploadFileOnCdn(MediaItem mediaItem)
+        {
+            var filePath = GetFilePath(mediaItem);
+            var mediaStream = mediaItem.GetMediaStream();
+            var exist = File.Exists(filePath);
+
+            if (exist)
+            {
+                ReplaceFileOnCdn(mediaItem);
+            }
+            else
+            {
+                var directoryPath = Path.GetDirectoryName(filePath);
+                if (directoryPath == null) return;
+                Directory.CreateDirectory(directoryPath);
+                using (var fileStream = File.Create(filePath))
+                {
+                    mediaStream.CopyTo(fileStream);
+                }
+                Log.Debug(string.Format(@"CDN has added: {0}", filePath), this);
+            }
+
+        }
+
+        public void ReplaceFileOnCdn(MediaItem mediaItem)
+        {
+            var filePath = GetFilePath(mediaItem);
+            var exist = File.Exists(filePath);
+
+            if (exist)
+            {
+                DeleteFileOnCdn(mediaItem);
+                UploadFileOnCdn(mediaItem);
+            }
+            else
+            {
+                UploadFileOnCdn(mediaItem);
+            }
+        }
+
+        public void DeleteFileOnCdn(MediaItem mediaItem)
+        {
+            var filePath = GetFilePath(mediaItem);
+            var exist = File.Exists(filePath);
+            if (exist)
+            {
+                File.Delete(filePath);
+            }
+            Log.Debug(string.Format(@"CDN has deleted: {0}", filePath), this);
+        }
+
+        [NotNull]
+        private string GetFilePath(MediaItem mediaItem)
+        {
+            var builder = new StringBuilder();
+            builder.Append(HttpRuntime.AppDomainAppPath.Remove(HttpRuntime.AppDomainAppPath.Length - 1));
+            builder.Append(CdnSourceDomain.Replace('/', '\\'));
+            builder.Append(mediaItem.MediaPath.Replace('/', '\\'));
+            builder.Append(".");
+            builder.Append(mediaItem.Extension);
+            return builder.ToString();
+        }
+
+        [NotNull]
+        private string GetCdnFolderPath(string cdnSourceDomain)
+        {
+            var builder = new StringBuilder();
+            builder.Append(HttpRuntime.AppDomainAppPath.Remove(HttpRuntime.AppDomainAppPath.Length - 1));
+            builder.Append(cdnSourceDomain.Replace('/', '\\'));
+            return builder.ToString();
+        }
+    }
+}
---
+++ CdnPublishing.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
+using Sitecore.Jobs;
+using Sitecore.Publishing.Pipelines.PublishItem;
+
+namespace SitecoreFromArg.CdnAzure
+{
+    public class CdnPublishing : PublishItemProcessor
+    {
+        public string Enabled { get; set; }
+        public string SourceDomain { get; set; }
+
+        public override void Process(PublishItemContext context)
+        {
+            if (Enabled.ToLower() != "yes") return;
+
+            Log.Debug("Performing CDN validations", this);
+
+            Assert.ArgumentNotNull(context, "context");
+            var contextItem = context.PublishHelper.GetSourceItem(context.ItemId);
+            if (contextItem == null) return;
+            if (!contextItem.Paths.IsMediaItem) return;
+            MediaItem mediaItem = contextItem;
+            if (string.IsNullOrEmpty(mediaItem.Extension)) return;
+            var mediaStream = mediaItem.GetMediaStream();
+            if (mediaStream == null || mediaStream.Length == 0) return;
+
+            var processor = new CdnAzureProcessor(SourceDomain);
+
+            Log.Debug("Starting CDN synchonization", this);
+
+            try
+            {
+                Sitecore.Context.Job.Status.State = JobState.Initializing;
+
+                if (context.Action == Sitecore.Publishing.PublishAction.None)
+                    processor.UploadFileOnCdn(mediaItem);
+
+                if ((context.Action == Sitecore.Publishing.PublishAction.PublishSharedFields) ||
+                    (context.Action == Sitecore.Publishing.PublishAction.PublishVersion))
+                    processor.ReplaceFileOnCdn(mediaItem);
+
+                if (context.Action == Sitecore.Publishing.PublishAction.DeleteTargetItem)
+                    processor.DeleteFileOnCdn(mediaItem);
+
+                Sitecore.Context.Job.Status.State = JobState.Finished;
+            }
+            catch (Exception ex)
+            {
+                var cdnEx = new Exception(string.Format("CDN Processing failed for {1} ({0}). {2}", contextItem.ID, contextItem.Name, ex.Message));
+                Log.Error(cdnEx.Message, cdnEx, this);
+
+                context.Job.Status.Failed = true;
+                context.Job.Status.Messages.Add(cdnEx.Message);
+            }
+
+            Log.Debug("CDN synchronization finished", this);
+        }
+    }
+}
